761540f9140b0666413480d39161c3ee2084c8d7
Muhamad Daffa Azfa Rabbani <109969988+Asoatram@users.noreply.github.com>
2024-09-29 20:54:39 +0700

    fix: updated struct file

--- case2-struct/main.go
+++ case2-struct/main.go
@@ -2,7 +2,7 @@
 
 package main
 
-import "fmt"
+import ("fmt")
 
 // Sebuah transaksi di toko buku akan disimpan ke dalam database.
 // Untuk itu, perlu dibuat model dari transaksi menggunakan struct.
@@ -22,13 +22,39 @@ import "fmt"
 // karangan Leila S. Chudori seharga 108000 sebanyak 1 buah. Buku berikutnya
 // berjudul "Bumi" karangan Tere Liye seharga 97000 sebanyak 1 buah.
 
-type Books struct{}
-type Transaction struct{}
+type Books struct{
+    Title string
+    Writer string
+    Price float64
+    Count int
+}
+type Transaction struct{
+    Total int
+    Date string
+    Book []Books
+}
 
 func main() {
 	// Modifikasi pendefinisian variable tx1 sehingga sesuai dengan kasus
 	// yang diceritakan.
-	tx1 := Transaction{}
+	tx1 := Transaction{
+	    Total: 32000,
+	    Date: "23-01-2021",
+	    Book: []Books{
+			{
+				Title:  "Laut Bercerita",
+				Writer: "Leila Salikha Chudori",
+				Price:  108000,
+				Count:  2,
+			},
+			{
+				Title:  "Bumi",
+				Writer: "Tere Liye",
+				Price:  97000,
+				Count:  1,
+			},
+		},
+	}
 
 	fmt.Println(tx1)
 }
